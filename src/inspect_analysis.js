65d943d534e0ad6e732ce3a22b194cf8627d80a0
Dennis <Dennisdenhollander@outlook.com>
2021-01-24 16:42:05 +0100

    Final changes

--- src/inspect_analysis.js
+++ src/inspect_analysis.js
@@ -14,22 +14,6 @@ function std(array) {
 
 (async () => {
   // placeholder
-  console.log("Average colour:");
-  for (let i = 0; i <= 5; i += 1) {
-    const filtered = Object.entries(avg_color)
-      .map((e) => ({
-        id: e[0],
-        rating: e[1].rating,
-        cluster_id: e[1].cluster_id,
-      }))
-      .filter((e) => e.cluster_id === `${i}`)
-      .map((e) => e.rating);
-
-    const stats = [i, filtered.length, _.mean(filtered), std(filtered)];
-    console.log(stats.join(","));
-  }
-
-  console.log("");
   console.log("Colour histogram:");
 
   for (let i = 0; i <= 5; i += 1) {
@@ -42,7 +26,14 @@ function std(array) {
       .filter((e) => e.cluster_id === `${i}`)
       .map((e) => e.rating);
 
-    const stats = [i, filtered.length, _.mean(filtered), std(filtered)];
+    const stats = [
+      i,
+      filtered.length,
+      _.mean(filtered),
+      std(filtered),
+      _.min(filtered),
+      _.max(filtered),
+    ];
     console.log(stats.join(","));
   }
 
@@ -59,7 +50,14 @@ function std(array) {
       .filter((e) => e.cluster_id === `${i}`)
       .map((e) => e.rating);
 
-    const stats = [i, filtered.length, _.mean(filtered), std(filtered)];
+    const stats = [
+      i,
+      filtered.length,
+      _.mean(filtered),
+      std(filtered),
+      _.min(filtered),
+      _.max(filtered),
+    ];
     console.log(stats.join(","));
   }
 })();
